84d0ae24742b048c219495ce1aaf0c7d073e664c
Michał Irzyłowski <michal.irzylowski@gmail.com>
2021-02-22 16:52:35 +0100

    add remove method

--- src/pages/house/house.tsx
+++ src/pages/house/house.tsx
@@ -1,14 +1,31 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { useParams } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+
+import * as actions from '@store/houses/actions';
+
+import { Button } from '@components/button';
 
 export const Hosue: React.FC = () => {
   const data = useSelector((state) => state.houses.houses);
   const params = useParams<{ id: string }>();
+  const dispatch = useDispatch();
 
   const house = data.find((hosueData) => hosueData._id === params.id);
 
+  const handleClick = useCallback(
+    () => dispatch(actions.deleteHouse.request(params.id)),
+    [dispatch, params.id]
+  );
+
   if (!house) return <div>there is no such a house</div>;
 
-  return <div>{house.createdAt}</div>;
+  return (
+    <div>
+      {house.createdAt}{' '}
+      <Button to="/houses" onClick={handleClick}>
+        Delete
+      </Button>
+    </div>
+  );
 };
--- src/store/saga/houses.saga.ts
+++ src/store/saga/houses.saga.ts
@@ -1,24 +1,47 @@
-import { all, put, takeLatest, call } from 'redux-saga/effects';
+import { all, put, takeLatest, call, select } from 'redux-saga/effects';
 import * as actions from '../houses/actions';
 import { House } from '@api/house';
 
-const requestUrl = 'http://mobile-reality-backend.sadek.usermd.net/houses/all';
+const requestUrl = 'http://mobile-reality-backend.sadek.usermd.net/houses/';
 
-const fetchApi = () =>
-  fetch(requestUrl)
+const fetchHouses = () =>
+  fetch(requestUrl + 'all')
     .then((res) => res.json())
     .then((data) => data.results as House[])
     .catch((err) => err);
 
 function* fetchHosues() {
   try {
-    const data = yield call(fetchApi);
+    const data = yield call(fetchHouses);
     yield put(actions.fetchHouses.success(data));
   } catch (error) {
     yield put(actions.fetchHouses.failure(error));
   }
 }
 
+const callDeleteHouse = (id: string) =>
+  fetch(requestUrl + id, { method: 'Delete' })
+    .then((data) => data)
+    .catch((err) => err);
+
+function* deleteHouse(action: any) {
+  const { houses } = yield select((state) => state);
+
+  try {
+    yield call(callDeleteHouse, action.payload);
+
+    const newHouses: House[] = houses.houses.filter(
+      (house: House) => house._id !== action.payload
+    );
+    yield put(actions.fetchHouses.success(newHouses));
+  } catch (error) {
+    console.log(error);
+  }
+}
+
 export default function* () {
-  yield all([takeLatest([actions.fetchHouses.request], fetchHosues)]);
+  yield all([
+    takeLatest([actions.fetchHouses.request], fetchHosues),
+    takeLatest([actions.deleteHouse.request], deleteHouse),
+  ]);
 }
